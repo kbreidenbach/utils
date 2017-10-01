ba1084bd5e0487a78a3c5af8fef9c8a1228d2982
Kevin Breidenbach <kevin.breidenbach@gmail.com>
2017-10-01 16:22:51 -0500

    initial commit

---
+++ src/main/java/me/breidenbach/util/Try.java
@@ -0,0 +1,96 @@
+package me.breidenbach.util;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+/**
+ * @author Kevin E. Breidenbach
+ * Date: 9/30/17
+ */
+@SuppressWarnings({"WeakerAccess", "UnusedReturnValue"})
+public class Try<T> {
+
+    private T payload;
+    private Throwable failure;
+
+    Try(TrySupplier<? extends T> supplier) {
+        try {
+            this.payload = supplier.supply();
+        } catch (Throwable throwable) {
+            failure = throwable;
+        }
+    }
+
+    Try(T payload) {
+        this.payload = payload;
+    }
+
+    Try(Throwable failure) {
+        this.failure = failure;
+    }
+
+    public Try<T> onSuccess(Consumer<? super T> successHandler) {
+        if (failure == null) {
+            successHandler.accept(payload);
+        }
+
+        return this;
+    }
+
+    public Try<T> onFailure(Consumer<Throwable> failureHandler) {
+        if (failure != null) {
+            failureHandler.accept(failure);
+        }
+
+        return this;
+    }
+
+    public Optional<T> get() {
+        return failure == null ? Optional.ofNullable(payload) : Optional.empty();
+    }
+
+    public Optional<Throwable> getError() {
+        return Optional.ofNullable(failure);
+    }
+
+    public Optional<T> getOrElse(T defaultValue) {
+        return failure == null ? Optional.ofNullable(payload) : Optional.ofNullable(defaultValue);
+    }
+
+    public <U> Try<U> map(Function<T, U> function) {
+        return failure == null ? new Try<>(() -> function.apply(payload)) : new Try<>(failure);
+    }
+
+    public <U> U fold(Function<Throwable, U> failureHandler, Function<T, U> successHandler) {
+        return failure == null ? successHandler.apply(payload) : failureHandler.apply(failure);
+    }
+
+    public Try<T> then(Consumer<Throwable> failureHandler, Consumer<? super T> successHandler) {
+        onSuccess(successHandler).onFailure(failureHandler);
+        return this;
+    }
+
+    public Stream<T> stream() {
+        return failure == null ?  Stream.of(payload) : Stream.empty();
+    }
+
+    public Try<T> forEach(Consumer<? super T> successHandler) {
+        stream().forEach(successHandler);
+        return this;
+    }
+
+    public boolean isFailure() {
+        return failure != null;
+    }
+
+    public boolean isSuccess() {
+        return failure == null;
+    }
+
+    @FunctionalInterface
+    public interface TrySupplier<T> {
+        T supply() throws Throwable;
+    }
+}
---
+++ src/test/java/me/breidenbach/util/TryTest.java
@@ -0,0 +1,204 @@
+package me.breidenbach.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Consumer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author Kevin E. Breidenbach
+ * Date: 9/30/17
+ */
+class TryTest {
+    private static final Throwable ERROR = new Exception("BAD TEST");
+    private static final String TEST = "TEST";
+
+    private int failureCount;
+    private int successCount;
+
+    private final Consumer<String> successTester = s -> {
+        assertThat(s, is(equalTo(TEST)));
+        successCount++;
+    };
+
+    private final Consumer<Throwable> failureTester = t -> {
+        assertThat(t, is(equalTo(ERROR)));
+        failureCount++;
+    };
+
+    @BeforeEach
+    void setUp() {
+        failureCount = 0;
+        successCount = 0;
+    }
+
+    @Test
+    void onSuccess() {
+        final Try<String> testTry = new Try<>(TEST);
+
+        testTry.onSuccess(successTester);
+
+        assertThat(successCount, is(equalTo(1)));
+        assertThat(testTry.isSuccess(), is(true));
+        assertThat(testTry.isFailure(), is(false));
+    }
+
+    @Test
+    void onFailure() {
+        final Try<String> testTry = new Try<>(ERROR);
+
+        testTry.onFailure(failureTester);
+
+        assertThat(failureCount, is(equalTo(1)));
+        assertThat(testTry.isSuccess(), is(false));
+        assertThat(testTry.isFailure(), is(true));
+    }
+
+    @Test
+    void thenSuccess() {
+        final TestType testType = new TestType(false);
+        final Try<String> testTry = new Try<>(testType);
+
+        testTry.then(failureTester, successTester);
+
+        assertThat(successCount, is(equalTo(1)));
+        assertThat(failureCount, is(equalTo(0)));
+        assertThat(testTry.isSuccess(), is(true));
+        assertThat(testTry.isFailure(), is(false));
+    }
+
+    @Test
+    void thenFailure() {
+        final TestType testType = new TestType(true);
+        final Try<String> testTry = new Try<>(testType);
+
+        testTry.then(failureTester, successTester);
+
+        assertThat(successCount, is(equalTo(0)));
+        assertThat(failureCount, is(equalTo(1)));
+        assertThat(testTry.isSuccess(), is(false));
+        assertThat(testTry.isFailure(), is(true));
+    }
+
+    @Test
+    void streamSuccess() {
+        final TestType testType = new TestType(false);
+        final Try<String> testTry = new Try<>(testType);
+
+        testTry.stream().forEach(successTester);
+
+        assertThat(successCount, is(equalTo(1)));
+    }
+
+    @Test
+    void streamFailure() {
+        final TestType testType = new TestType(true);
+        final Try<String> testTry = new Try<>(testType);
+
+        testTry.stream().forEach(successTester);
+
+        assertThat(successCount, is(equalTo(0)));
+    }
+
+    @Test
+    void forEachSuccess() {
+        final TestType testType = new TestType(false);
+        final Try<String> testTry = new Try<>(testType);
+
+        testTry.forEach(successTester);
+
+        assertThat(successCount, is(equalTo(1)));
+    }
+
+    @Test
+    void forEachFailure() {
+        final TestType testType = new TestType(true);
+        final Try<String> testTry = new Try<>(testType);
+
+        testTry.forEach(successTester);
+
+        assertThat(successCount, is(equalTo(0)));
+    }
+
+    @Test
+    void mapSuccess() {
+        final TestType testType = new TestType(false);
+        final Try<String> testTry = new Try<>(testType);
+        final Try<Integer> result = testTry.map(String::length);
+
+        assertThat(result.isSuccess(), is(true));
+        assertThat(result.isFailure(), is( false));
+        assertThat(result.get().isPresent(), is(true));
+        assertThat(result.getError().isPresent(), is(false));
+        assertThat(result.get().get(), is(equalTo(TEST.length())));
+    }
+
+    @Test
+    void mapFailure() {
+        final TestType testType = new TestType(true);
+        final Try<String> testTry = new Try<>(testType);
+        final Try<Integer> result = testTry.map(String::length);
+
+        assertThat(result.isSuccess(), is(false));
+        assertThat(result.isFailure(), is( true));
+        assertThat(result.get().isPresent(), is(false));
+        assertThat(result.getError().isPresent(), is(true));
+        assertThat(result.getError().get(), is(equalTo(ERROR)));
+    }
+
+    @Test
+    void getOrElseSuccess() {
+        final TestType testType = new TestType(false);
+        final Try<String> testTry = new Try<>(testType);
+        final String defaultValue = "DEFAULT";
+
+        assertThat(testTry.getOrElse(defaultValue).isPresent(), is(true));
+        assertThat(testTry.getOrElse(defaultValue).get(), is(equalTo(TEST)));
+    }
+
+    @Test
+    void getOrElseFailure() {
+        final TestType testType = new TestType(true);
+        final Try<String> testTry = new Try<>(testType);
+        final String defaultValue = "DEFAULT";
+
+        assertThat(testTry.getOrElse(defaultValue).isPresent(), is(true));
+        assertThat(testTry.getOrElse(defaultValue).get(), is(equalTo(defaultValue)));
+    }
+
+    @Test
+    void foldSuccess() {
+        final TestType testType = new TestType(false);
+        final Try<String> testTry = new Try<>(testType);
+        final int result = testTry.fold(t -> 0, s-> 1);
+
+        assertThat(result, is(equalTo(1)));
+    }
+
+    @Test
+    void foldFailure() {
+        final TestType testType = new TestType(true);
+        final Try<String> testTry = new Try<>(testType);
+        final int result = testTry.fold(t -> 0, s-> 1);
+
+        assertThat(result, is(equalTo(0)));
+    }
+
+    private class TestType implements Try.TrySupplier<String> {
+        private final boolean fail;
+
+        private TestType(boolean fail) {
+            this.fail = fail;
+        }
+
+        @Override
+        public String supply() throws Throwable {
+            if (fail) throw ERROR;
+            return TEST;
+        }
+    }
+}
