03e026141a152f216e7c8cfe074d4808f6abf118
Kevin Breidenbach <kevin.breidenbach@gmail.com>
2017-10-08 21:57:05 -0500

    Enhancements for For Comprehension with Try types

--- src/test/java/me/breidenbach/util/ForCompTest.java
+++ src/test/java/me/breidenbach/util/ForCompTest.java
@@ -3,15 +3,12 @@ package me.breidenbach.util;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static me.breidenbach.util.ForComp.forFunction;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 /**
